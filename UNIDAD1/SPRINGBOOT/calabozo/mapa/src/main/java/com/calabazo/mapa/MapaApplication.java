3d0fa016953c5ea8f0a3551324e2f25f367bb60c
Mario <oiramdeveloper@gmail.com>
2025-10-03 11:25:22 +0200

    actu

--- UNIDAD1/SPRINGBOOT/calabozo/mapa/src/main/java/com/calabazo/mapa/MapaApplication.java
+++ UNIDAD1/SPRINGBOOT/calabozo/mapa/src/main/java/com/calabazo/mapa/MapaApplication.java
@@ -10,4 +10,6 @@ public class MapaApplication {
 		SpringApplication.run(MapaApplication.class, args);
 	}
 
+
+
 }
--- UNIDAD1/SPRINGBOOT/calabozo/mapa/src/main/java/com/calabazo/mapa/api/ApiCiudadController.java
+++ UNIDAD1/SPRINGBOOT/calabozo/mapa/src/main/java/com/calabazo/mapa/api/ApiCiudadController.java
@@ -2,15 +2,18 @@ package com.calabazo.mapa.api;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.calabazo.mapa.model.Ciudad;
 import com.calabazo.mapa.repository.CiudadRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/ciudades")
@@ -28,5 +31,34 @@ public class ApiCiudadController {
         // Al ser un restController se convertira a json
         return ciudadRepository.findAll();
     }
+    	/*
+	 * Creamos en endpong para saccar una unica entidad 
+	 * A la hora de reciirparmetros se pueden coger como  parametros rl
+	 * o parametro get get , es decir vayan direcatmente en la url o que se a con elformato 
+	 * /api/ciudades?sexo=M%ciudad=madrid
+	 * 
+	 */
+    @GetMapping("/{id}")
+    public ResponseEntity<Ciudad> getCiudadById(@PathVariable Long id) {
+        /*
+         * La funcion devuelve un ResponseEntity a la solicitud de la api , es una respuesta completa
+         * con multiples configuraciones
+         * primero sacamos la ciudad asociada a dicho id y si existe mapeamos la salida a una responseEntity
+         * que tiene como cuerpo los datos de la entidad ciudad del id 
+         * Si no existe nos devuelve notFound, es obligatorio poner orElse ya que map devuelve un optional , es decir algo opcional
+         * 
+         * 
+         */
+        return ciudadRepository.findById(id).map(ciudad -> ResponseEntity.ok().body(ciudad)
+        .orElse(ResponseEntity.noContent().build()));
+    }
+    @PostMapping("/crear")
+    public Ciudad createCity(@RequestBody Ciudad ciudad) {
+        //TODO: process POST request
+        
+        return ciudadRepository.save(ciudad);
+    }
+    
+    
 
 }
